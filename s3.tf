d1b762750dfb1819f92a730277f7f4dd07fcc2ba
HarikrishnaSiram <siramharikrishna@gmail.com>
2024-08-19 17:02:59 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -22,11 +22,3 @@ versioning_configuration {
 status = "Enabled"
 }
 }
-
-terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "mustafaccit.devops.project.bucket"
-key = "prod/terraform.tfstate"
-}
-}
