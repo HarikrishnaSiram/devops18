216a922b539d7961abf31ea27bd54cccd6896faf
HarikrishnaSiram <siramharikrishna@gmail.com>
2024-09-01 11:40:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ resource "aws_launch_configuration" "web_server_as" {
     image_id           = "ami-02c21308fed24a8ab"
     instance_type = "t2.micro"
     key_name = "KeyPair"
-tags = {
-Name = "web-server"
-grp = "asg1"
-}
 }
    
 
