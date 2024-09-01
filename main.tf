b07ce6c37f34404fa4019e51d841e7f4d583a077
HarikrishnaSiram <siramharikrishna@gmail.com>
2024-09-01 11:42:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ resource "aws_launch_configuration" "web_server_as" {
     instance_type = "t2.micro"
     key_name = "KeyPair"
 }
+
+resource "aws_autoscaling_tag" "web_server_as_tag" {
+  key                 = "grp"
+  value               = "asg1"
+  propagate_at_launch = true
+  resource_id         = aws_launch_configuration.web_server_as.name
+  resource_type       = "launch_configuration"
+}
    
 
 
